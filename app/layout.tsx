e1fe7aa62bab617b47f8b7922bc603589c09b157
kim young jae <jae2942@gmail.com>
2024-03-05 17:39:05 +0900

    CHORE : tailwind css hmoe add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +12,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-gray-500 text-white'>{children}</body>
     </html>
   );
 }
