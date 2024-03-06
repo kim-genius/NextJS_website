5dff85a9c3df4dfa1f245c7bdd2ca91a145fd50d
kim young jae <jae2942@gmail.com>
2024-03-06 17:10:05 +0900

    CHORE : list map add

--- app/list/page.jsx
+++ app/list/page.jsx
@@ -1,14 +1,17 @@
 
 export default function List() {
+    let 상품 = [['carrot','100$'],['carcume','150$'],['bluebarry','100$'],['strawberry','40$']]
     return (
         <div className="text-center">
-        <h2>Products</h2>
-        <div className="m-20 mt-10 m-auto w-80 bg-white text-black p-10 rounded-md">
-          <h4>상품명 $40</h4>
-        </div>
-        <div className="m-20 mt-10 m-auto w-80 bg-white text-black p-10 rounded-md">
-          <h4>상품명 $40</h4>
-        </div>
+                    <h2>Products</h2>
+            {        
+                상품.map((i)=>{
+                    return(
+                <div className="m-20 mt-10 m-auto w-80 bg-white text-black p-10 rounded-md">
+                    <h4>{i[0]} {i[1]}</h4>
+                  </div>
+                           )})
+            }
       </div>
     );
   }
