fd7625f1537ca98cca33d819884f14daa4acbdf2
kim young jae <jae2942@gmail.com>
2024-03-07 19:06:56 +0900

    CHORE : each products count add

--- app/components/Cartitem.jsx
+++ app/components/Cartitem.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import 블루베리 from '/public/image/bluebarry.jpeg'
 
 export default function Cartitem ({상품}){
-    const [상품개수,set상품개수] = useState(0)
+    const [상품개수,set상품개수] = useState([3,0,0,0])
     return(
         <div>
         <h2>Products</h2>
@@ -16,8 +16,15 @@ export default function Cartitem ({상품}){
                     <Image src = {블루베리}></Image>
                         <div className="m-20 mt-10 m-auto w-80 bg-white text-black p-10 rounded-md" key={index}>
                             <h4>{i[0]} {i[1]}</h4>
-                            <span>{상품개수}</span>
-                            <button onClick={()=>set상품개수(상품개수+1)}>+</button>
+                            <span>{상품개수[index]}</span>
+                            <button className="ml-5" onClick={()=>{
+                                let copy = [...상품개수]
+                                copy[index]++
+                                return(set상품개수(copy))}}>+</button>
+                            <button className="ml-5" onClick={()=>{
+                                let copy = [...상품개수]
+                                copy[index]--
+                                return(set상품개수(copy))}}>-</button>
                         </div>
                      </div>
                        )})
--- app/list/page.jsx
+++ app/list/page.jsx
@@ -1,5 +1,5 @@
 
-import Cartitem from '../components/page'
+import Cartitem from '../components/Cartitem'
 export default function List() {
     let 상품 = [['carrot','100$'],['carcume','150$'],['bluebarry','100$'],['strawberry','40$']]
     return (
