c8494233d58d4734eda6b574cdcffbc00cbbf9cb
kim young jae <jae2942@gmail.com>
2024-03-10 18:52:24 +0900

    FEAT : dynamic route add

---
+++ app/post/detail/[route]/page.jsx
@@ -0,0 +1,14 @@
+import { connectDB } from '../../../../utils/database'
+import { ObjectId } from 'mongodb';
+export default async function Detail(props) {
+    
+    const client = await connectDB;
+    const db = client.db('forum')
+     let result = await db.collection('post').findOne({_id: new ObjectId(props.params.route)})
+  return (
+    <div className="bg-white p-10 flex flex-col">
+      <h1 className="text-3xl text-black">{result.title}</h1>
+      <p className="text-lg text-black">{result.content}</p>
+  </div>
+  );
+}
--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,10 +1,11 @@
 import { connectDB } from '../../utils/database'
 import Postcreate from '../components/Postcreate'
+import Link from 'next/link';
 export default async function Post() {
     const client = await connectDB;
     const db = client.db('forum')
      let result = await db.collection('post').find().toArray()
-    
+        console.log(result)
   return (
     <div className="bg-gray-50 p-10">
         <Postcreate></Postcreate>
@@ -12,8 +13,10 @@ export default async function Post() {
             result.map((a,index)=>{
                 return(
                     <div className="bg-white rounded-lg p-20 mb-5 shadow-md " key={index}>
+                    <Link href={`/post/detail/${a._id}`}>
                     <h4 className="text-3xl font-extrabold m-0 text-black">{a.title}</h4>
                     <p className="text-lg text-gray-400 my-5 mx-0">{a.content}</p>
+                    </Link>
                   </div>
                 )
             })
