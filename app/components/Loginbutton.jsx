89439731999dc06742d208ae0cb17e452b5641d6
kim young jae <jae2942@gmail.com>
2024-03-18 15:27:26 +0900

    CHORE : oauth sesstion login add

--- app/components/Loginbutton.jsx
+++ app/components/Loginbutton.jsx
@@ -4,7 +4,7 @@ import {signIn, signOut } from "next-auth/react"
 export default function Loginbutton() {
   return (
     <>
-        <button className="font-white" onClick={() => signIn()}>Sign in</button>
+        <button className="font-white" onClick={() => signIn()}>Github Sign in</button>
     </>
 
   );
--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { connectDB } from "@/utils/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -7,7 +9,8 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  secret : 'qwer1234',
+  adapter : MongoDBAdapter(connectDB),
 }
 export default NextAuth(authOptions)
