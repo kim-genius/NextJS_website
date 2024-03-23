e5f421a152639a5b06894c03c6f18ee2ba7a0b1f
kim young jae <jae2942@gmail.com>
2024-03-23 19:14:36 +0900

    FIX : comment lsit 0 add

--- app/components/comment.jsx
+++ app/components/comment.jsx
@@ -10,21 +10,23 @@ export default function Comment({postId}) {
   useEffect(()=>{
     axios.get(`/api/commentList?postId=${postId}`)
     .then((res)=>{return(setCommentList(res.data),console.log('뭐나옴?',comment))})
-  },[comment,setComment,commentList,setCommentList])
+  },[])
 
   return (
       <div className="flex flex-col text-black border-solid border-black border-2">
         <div className="text-center">댓글목록</div>
-        <div className="bg-gray w-1/2 h-1/2">
-          {commentList.map((a,index)=>{
+        <div className="bg-gray w-full h-1/2">
+          {commentList.length > 0? commentList.map((a,index)=>{
             return(
-            <div className="bg-gray text-black flex content-center " key={index}>
-            <p className="text-xl">{a.content}</p>
-            <span className="text-sm text-slate-300 ml-2">{a.writer}</span>
-            <hr className="mt-2 text-slate-300"></hr>
+            <div className="w-full bg-gray text-black flex flex-col p-4 content-center" key={index}>
+              <div className="flex w-full">
+                <p className="w-1/2 text-xl">{a.content}</p>
+                <span className="text-sm text-slate-300 ml-2 ">{a.writer}</span>
+            </div>
+            <hr className="text-center mt-3 w-7/12 bg-slate-500"></hr>
             </div>
             )
-          })}
+          }) :'댓글없음'}
         </div>
         <input onChange={(e)=>{setComment(e.target.value)}} value={comment}/>
         <button onClick={()=>{
