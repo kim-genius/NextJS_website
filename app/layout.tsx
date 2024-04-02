ac9cfa24196a135d41e6a15d69fab7717e027610
kim young jae <jae2942@gmail.com>
2024-04-02 18:47:28 +0900

    FEAT : server actions add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@
 import "./globals.css";
 import Link from "next/link";
 import Darkmode from "../app/components/Darkmode"
-
+import handleSubmit from './components/Handlesubmit'
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   
-  
+
 
   return (
     <html lang="en">
@@ -19,6 +19,7 @@ export default function RootLayout({
           <Link className="mr-10" href="/">home</Link>
           <Link className="mr-10" href="/list">list페이지</Link>
           <Link className="mr-10" href="/post">post페이지</Link>
+          <form action={handleSubmit}><button>버튼</button></form>
         </div>
         
         <div className="w-full flex justify-end mr-4">
